2b72a3d6933ad54c2b9300385547bc03dab0a2ba
Kirkhammetz <simonecorsi.rm@gmail.com>
2018-08-02 00:31:26 +0200

    persoanl settings

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -1,33 +1,57 @@
 from instapy import InstaPy
+import env
 
 # Write your automation here
 # Stuck ? Look at the github page or the examples in the examples folder
 
-insta_username = ''
-insta_password = ''
+insta_username = env.insta_username
+insta_password = env.insta_password
 
-dont_like = ['food', 'girl', 'hot']
-ignore_words = ['pizza']
-friend_list = ['friend1', 'friend2', 'friend3']
+dont_like = env.dont_like
+ignore_words = env.ignore_words
+friend_list = env.friend_list
 
-# If you want to enter your Instagram Credentials directly just enter
-# username=<your-username-here> and password=<your-password> into InstaPy
-# e.g like so InstaPy(username="instagram", password="test1234")
-
-bot = InstaPy(username=insta_username, password=insta_password, selenium_local_session=False)
-bot.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
+bot = InstaPy(
+    username=insta_username,
+    password=insta_password,
+    selenium_local_session=False,
+    headless_browser=True,
+)
+bot.set_selenium_remote_session(selenium_url="http://selenium:4444/wd/hub")
 bot.login()
-bot.set_relationship_bounds(enabled=True,
-             potency_ratio=-1.21,
-              delimit_by_numbers=True,
-               max_followers=4590,
-                max_following=5555,
-                 min_followers=45,
-                  min_following=77)
-bot.set_do_comment(True, percentage=10)
-bot.set_comments(['Cool!', 'Awesome!', 'Nice!'])
+bot.set_relationship_bounds(
+    enabled=True,
+    potency_ratio=1.5,
+    delimit_by_numbers=True,
+    # max_followers=300000,
+    min_followers=750,
+    # max_following=5000,
+    # min_following=500,
+)
+bot.set_delimit_liking(enabled=True, max=None, min=10)
+
+bot.set_use_clarifai(enabled=True, api_key="fb4a4026cc2947edb7cc8e7ce67dbeca")
 bot.set_dont_include(friend_list)
 bot.set_dont_like(dont_like)
 bot.set_ignore_if_contains(ignore_words)
-bot.like_by_tags(['dog', '#cat'], amount=100)
+bot.set_user_interact(amount=3, randomize=True, percentage=100, media="Photo")
+bot.like_by_tags(env.like_by_tags, amount=100, interact=True, media="Photo")
+session.like_by_feed(amount=100, randomize=True, unfollow=True, interact=True)
+
+# avoid activity on
+
+
+bot.set_dont_unfollow_active_users(enabled=True, posts=3)
+
+bot.unfollow_users(
+    amount=60,
+    InstapyFollowed=(True, "nonfollowers"),
+    style="FIFO",
+    unfollow_after=55 * 60 * 60,
+    sleep_delay=501,
+)
+
+bot.clarifai_check_img_for(env.clarifai_check_img_for)
+
 bot.end()
+
