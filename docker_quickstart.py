ccb7e867375847dfe1f9cd522c18fa8ef21dffb5
Kirkhammetz <simonecorsi.rm@gmail.com>
2018-08-21 21:59:23 +0200

    latest changes

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -7,26 +7,17 @@ from proxy_extension import create_proxy_extension
 import random
 
 
-# import random
-# random.choice(array)
-
-dont_like = env.dont_like
-ignore_words = env.ignore_words
-friend_list = env.friend_list
-hashtags = env.hashtags
-
-
-def job(cursor=0):
-    current_hashtag = hashtags[cursor : cursor + 1]
+def job(cursor=0, username=env.username, passsword=env.passsword):
+    current_hashtag = env.hashtags[cursor : cursor + 1]
     bot = InstaPy(
-        username=env.username,
-        password=env.passsword,
+        username=username,
+        password=password,
         selenium_local_session=False,
         headless_browser=True,
         bypass_suspicious_attempt=True,
         multi_logs=True,
-        # proxy_address="212.237.52.87",
-        # proxy_port=443,
+        proxy_address="212.237.52.87",
+        proxy_port=443,
     )
     try:
         bot.set_selenium_remote_session(selenium_url="http://selenium:4444/wd/hub")
@@ -37,49 +28,36 @@ def job(cursor=0):
             delimit_by_numbers=True,
             max_followers=10000,
             max_following=3000,
-            min_followers=500,
+            min_followers=400,
             min_following=50,
         )
-        # bot.set_delimit_liking(enabled=True, max=None, min=10)
-        # bot.set_use_clarifai(enabled=True, api_key=env.clarifai_apikey)
-        # bot.set_dont_include(friend_list)
-        # bot.set_dont_like(dont_like)
-        # bot.set_ignore_if_contains(ignore_words)
-        # # bot.set_dont_unfollow_active_users(enabled=True, posts=3)
-        # bot.set_user_interact(
-        #     amount=random.randint(1, 3), randomize=True, percentage=20, media="Photo"
-        # )
-        # bot.set_do_follow(enabled=True, percentage=5, times=1)
 
-        # # bot.set_delimit_commenting(enabled=True, max=32, min=3)
-        # # bot.set_smart_hashtags(['portraitpage', 'portraitgames', 'agameof10k', 'a], limit=3, sort='top', log_tags=True)
-        # # bot.clarifai_check_img_for(['landscape'], comment=True, comments=['Awesome shot!😄', 'Nice shot!😄', 'Great!😄', 'Nice one', 'Awesome', 'Cool!'])
-        # # bot.like_by_locations(['31499759/rome-italy'], amount=10, skip_top_posts=False)
-        # bot.follow_user_followers(["portraitgames"], amount=10, randomize=False)
-        # # bot.follow_by_tags(env.hashtags, amount=10)
-        # bot.like_by_feed(amount=50, randomize=True, unfollow=True, interact=True)
         bot.clarifai_check_img_for(env.clarifai_check_img_for)
-        bot.set_dont_include(friend_list)
-        bot.set_dont_like(dont_like)
-        bot.set_ignore_if_contains(ignore_words)
+        bot.set_dont_include(env.friend_list)
+        bot.set_dont_like(env.dont_like)
         bot.set_user_interact(
-            amount=random.randint(1, 3), randomize=True, percentage=20, media="Photo"
+            amount=random.randint(1, 3), randomize=True, percentage=30, media="Photo"
         )
+
+        bot.like_by_tags(
+            current_hashtag, amount=random.randint(2, 5), interact=True, media="Photo"
+        )
+        session.like_by_feed(amount=100, randomize=random.randint(2, 5), interact=True)
+
         bot.follow_user_followers(
-            ["portraitgames", "portraits_vision", "portraitvision"],
-            amount=10,
-            randomize=False,
+            env.follow_userbase,
+            randomize=True,
             interact=True,
-            sleep_delay=60,
+            amount=random.randint(5, 10),
         )
-        bot.follow_by_tags(current_hashtag, amount=20)
-        bot.like_by_tags(current_hashtag, amount=20, interact=True, media="Photo")
+
+        bot.follow_by_tags(current_hashtag, amount=random.randint(5, 25))
         bot.unfollow_users(
-            amount=100,
+            amount=random.randint(25, 50),
             InstapyFollowed=(True, "nonfollowers"),
-            style="FIFO",
-            unfollow_after=12 * 60 * 60,
-            sleep_delay=60,
+            style="RANDOM",
+            unfollow_after=24 * 60 * 60,
+            sleep_delay=655,
         )
     except expression as identifier:
         print(traceback.format_exc())
@@ -87,19 +65,13 @@ def job(cursor=0):
         bot.end()
         wait = 60 * 60  # 1hr
         cursor += 1
-        if cursor == len(hashtags) - 1:
+        if cursor == len(env.hashtags) - 1:
             cursor = 0
-            print("rewinding hastags, starting over in 4hrs")
-            time.sleep(wait * 4)
+            print("rewinding hastags, starting over in 3hrs")
+            time.sleep(wait * 3)
         else:
             time.sleep(wait)
         return job(cursor)
 
 
 job()
-# schedule.every(4).hours.do(job).run()
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-
