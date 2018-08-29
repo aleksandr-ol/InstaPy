cb17e114e72b1c41cb2f0591fed31bf07e63109b
Kirkhammetz <simonecorsi.rm@gmail.com>
2018-08-29 11:08:18 +0200

    fixed

---
+++ instapy/multiaccount.py
@@ -0,0 +1,85 @@
+import atexit
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from instapy import InstaPy
+import datetime
+import traceback
+import time
+import random
+# from bson.objectid import ObjectId
+from .mongodb import Database
+from .bot import Bot
+import multiprocessing
+
+MongoDB = Database(
+    host=os.getenv("MONGODB_HOST"),
+    port=int(os.getenv("MONGODB_PORT")),
+    database=os.getenv("MONGODB_DB"),
+)
+
+# restart active when main process restart from crash
+
+
+def stop_all_bot():
+    Account = MongoDB.get_collection("instagram_account")
+    Account.update_many({"botStatus": "active"}, {
+                        "$set": {"botStatus": "restart"}})
+    # if a bot is set as paused but without untild schedule for restart
+    # this mainly due to error.
+    # Account.update_many({"botStatus": "paused", "$or": [{"pausedUntil": {"$exists": False}}, {"pausedUntil": None}]}, {
+    #                     "$set": {"botStatus": "restart"}})
+
+
+def exit_handler():
+    print("Exiting..")
+    try:
+        stop_all_bot()
+    except Exception as error:
+        print(str(error))
+        pass
+
+
+atexit.register(exit_handler)
+Account = MongoDB.get_collection("instagram_account")
+User = MongoDB.get_collection("user")
+
+
+def run():
+    while True:
+        print("[BotsManager]: Looking for stopped account to run...")
+        Bot.on_script_start(MongoDB)
+        query = {
+            "$or": [
+                {"botStatus": "restart"},
+                {"botStatus": "paused", "pausedUntil": {
+                    "$lte": datetime.datetime.now()}},
+            ],
+            # "expiresOn": {"$lt": datetime.datetime.now()}
+        }
+        for account in Account.find(query):
+            user = User.find_one({"_id": account["user"]})
+            account['user'] = user
+            print("found inactive account", account["username"])
+            if user and account:
+                try:
+                    bot = Bot(account=account)
+                    process = multiprocessing.Process(
+                        target=bot.start_bot_session)
+                    print(
+                        "MULTI - Starting user: %s at %s"
+                        % (
+                            account["username"],
+                            datetime.datetime.now().strftime("%H:%M:%S"),
+                        )
+                    )
+                    process.start()
+                    # process.join()
+                    bot.set_bot_status("active")
+                except Exception as error:
+                    print("[BotsManager]: Error - ", str(error))
+                    bot.set_bot_status("stopped")
+
+        time.sleep(10)
