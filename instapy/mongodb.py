6c4a83371b55b6d4211f9d3147eee7cea4c197af
Kirkhammetz <simonecorsi.rm@gmail.com>
2018-09-24 22:02:17 +0200

    fix

--- instapy/mongodb.py
+++ instapy/mongodb.py
@@ -4,13 +4,16 @@ import os
 
 class Database(object):
     def __init__(self, host="localhost", port=27017, database="my_databse"):
-        self.db_client = pymongo.MongoClient(host, int(port),
-                                             username=os.getenv(
-                                                 "MONGODB_USER", None),
-                                             password=os.getenv(
-                                                 "MONGODB_PWD", None)
-                                             )
-        self.db = self.db_client[database]
+        try:
+            self.db_client = pymongo.MongoClient(host, port=int(port),
+                                                 username=os.getenv(
+                "MONGODB_USER", None),
+                password=os.getenv(
+                "MONGODB_PWD", None)
+            )
+            self.db = self.db_client[database]
+        except Exception as error:
+            print(error)
 
     def get_database(self):
         return self.db
