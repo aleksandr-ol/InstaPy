b76e80fde9c70da3e35095b432900b7a9c2f782a
Kirkhammetz <simonecorsi.rm@gmail.com>
2018-09-24 21:48:01 +0200

    added_auth_mongo

--- instapy/mongodb.py
+++ instapy/mongodb.py
@@ -1,9 +1,15 @@
 import pymongo
+import os
 
 
 class Database(object):
     def __init__(self, host="localhost", port=27017, database="my_databse"):
-        self.db_client = pymongo.MongoClient(host, int(port))
+        self.db_client = pymongo.MongoClient(host, int(port),
+                                             username=os.getenv(
+                                                 "MONGODB_USER", None),
+                                             password=os.getenv(
+                                                 "MONGODB_PWD", None)
+                                             )
         self.db = self.db_client[database]
 
     def get_database(self):
