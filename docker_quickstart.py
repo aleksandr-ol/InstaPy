bff89a7751496ddfb2997f26d7db24e448f5b43e
Kirkhammetz <simonecorsi.rm@gmail.com>
2018-09-24 09:25:35 +0200

    cleanup

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -1,12 +1,3 @@
 
 from instapy import multiaccount
 multiaccount.run()
-
-"""
-  TODO:
-  Implement a process fork on each bot that 
-  will watch for changes in accunt configuration and
-  dispatch action based on that
-  eg: if backend for some reason stop the both
-  we should call session.end()
-"""
