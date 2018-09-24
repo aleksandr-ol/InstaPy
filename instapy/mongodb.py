ac39d71102fb5bbe23b6559773a4b39654077b38
Kirkhammetz <simonecorsi.rm@gmail.com>
2018-09-24 22:23:51 +0200

    mongoconnection

--- instapy/mongodb.py
+++ instapy/mongodb.py
@@ -4,13 +4,14 @@ import os
 
 class Database(object):
     def __init__(self, host="localhost", port=27017, database="my_databse"):
+        username = os.getenv(
+            "MONGODB_USER", "")
+        password = os.getenv(
+            "MONGODB_PWD", "")
+        constr = "%s:%s@%s:%s/%s" % (username,
+                                     password, host, int(port), database)
         try:
-            self.db_client = pymongo.MongoClient(host, port=int(port),
-                                                 username=os.getenv(
-                "MONGODB_USER", None),
-                password=os.getenv(
-                "MONGODB_PWD", None)
-            )
+            self.db_client = pymongo.MongoClient(constr)
             self.db = self.db_client[database]
         except Exception as error:
             print(error)
